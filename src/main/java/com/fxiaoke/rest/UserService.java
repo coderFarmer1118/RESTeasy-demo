502eb9876ea57780b58ea7e8d1fe826e467a97a8
unknown <wangzhx@firstshare.cn>
2017-12-26 11:57:14 +0800

    new four User

--- src/main/java/com/fxiaoke/rest/UserService.java
+++ src/main/java/com/fxiaoke/rest/UserService.java
@@ -5,9 +5,19 @@ import java.util.Map;
 
 public class UserService {
 
-    static Map<Integer,Object> map = new HashMap<>();
+    /**
+     * 初始化四个用户
+     */
+   private static Map<Integer, Object> map = new HashMap<>();
 
     static {
-
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+        User user4 = new User();
+        map.put(1, user1);
+        map.put(2, user2);
+        map.put(3, user3);
+        map.put(4, user4);
     }
 }

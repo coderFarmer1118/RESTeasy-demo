2c86f93ca95b5c83f7cdc023e4475dcb56ac882c
unknown <wangzhx@firstshare.cn>
2017-12-26 11:20:47 +0800

    RESTeassy

---
+++ src/main/java/com/fxiaoke/rest/UserService.java
@@ -0,0 +1,13 @@
+package com.fxiaoke.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserService {
+
+    static Map<Integer,Object> map = new HashMap<>();
+
+    static {
+
+    }
+}
